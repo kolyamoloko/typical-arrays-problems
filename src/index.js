29964472a50c1e242db8197a61f555424ec4964f
kolyamoloko <kolyamoloko@mail.ru>
2021-08-19 15:42:24 +0300

    add: solution function min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length){let i = 0;
+    m = array[0];
+   while (i < array.length){
+     if (m >= array[i]){
+       m = array[i]
+     }
+     i++
+   }
+   return m;
+  }
+else {
+  return 0
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length){let i = 0;
+    m = array[0];
+   while (i < array.length){
+     if (m <= array[i]){
+       m = array[i]
+     }
+     i++
+   }
+   return m;}
+  else {
+    return 0;  
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length){let i = 0;
+    let m = 0;
+    while (i < array.length){
+      m = m + array[i]
+      i++
+    }
+    return (m / array.length);}
+  else{
+    return 0;
+  }
 }
